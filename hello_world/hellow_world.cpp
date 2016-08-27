8d8ed2a221aa19691955df6440700e70cf8aef8f
Christopher D. McMurrough <christopher.mcmurrough@gmail.com>
2016-08-27 13:52:28 -0500

    added hello world example

---
+++ hello_world/hellow_world.cpp
@@ -0,0 +1,24 @@
+/*******************************************************************************************************************//**
+* @file hello_world.cpp
+* @brief simple hello world example in C++
+* @author Christopher D. McMurrough
+***********************************************************************************************************************/
+
+// include necessary dependencies
+#include <iostream>
+
+/*******************************************************************************************************************//**
+* @brief program entry point
+* @param[in] argc number of command line arguments
+* @param[in] argv string array of command line arguments
+* @return return code (0 for normal termination)
+* @author Christoper D. McMurrough
+***********************************************************************************************************************/
+int main(int argc, char **argv)
+{
+    // print message to console
+    std::cout << "Hello World!" << std::endl;
+
+    // terminate the program
+    return 0;
+}
