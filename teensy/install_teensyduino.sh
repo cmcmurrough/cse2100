2fc662e7c4afa7861fdf287feb4c480c2bad7c0b
Christopher D. McMurrough <christopher.mcmurrough@gmail.com>
2016-10-04 22:45:53 -0500

    script fix

--- teensy/install_teensyduino.sh
+++ teensy/install_teensyduino.sh
@@ -1,7 +1,7 @@
 # install the Arduino IDE
-wget https://www.arduino.cc/download_handler.php?f=/arduino-1.6.12-linuxarm.tar.xz
-tar xf arduino-1.6.12-linuxarm.tar.xz
-sh install.sh
+#wget https://www.arduino.cc/download_handler.php?f=/arduino-1.6.12-linuxarm.tar.xz
+#tar xf arduino-1.6.12-linuxarm.tar.xz
+#sh install.sh
 
 # download and install the udev rules
 wget https://www.pjrc.com/teensy/49-teensy.rules
